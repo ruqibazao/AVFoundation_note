ead6a8dd600a923ed5b52ebe1e180f1ab6dd5aa0
nenhall_work <nenhall@126.com>
2019-01-17 16:37:24 +0800

    first commit

---
+++ AVFoundation_note/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVFoundation_note
+//
+//  Created by nenhall on 2019/1/16.
+//  Copyright © 2019 nenhall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AVFoundation_note/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVFoundation_note
+//
+//  Created by nenhall on 2019/1/16.
+//  Copyright © 2019 nenhall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
